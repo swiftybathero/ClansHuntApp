a1e2806f30c3277004c68c23b1f91548a3bc67f1
swiftybathero <swiftybathero@gmail.com>
2017-12-11 21:50:23 +0100

    First version of DB

---
+++ ClansHuntApp.Database/dbo/Tables/Member.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Member] (
+    [ID]   INT           IDENTITY (1, 1) NOT NULL,
+    [Tag]  NVARCHAR (12) NOT NULL,
+    [Name] NVARCHAR (50) NOT NULL,
+    CONSTRAINT [PK_Member] PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
+
+GO
+CREATE UNIQUE NONCLUSTERED INDEX [UX_Member]
+    ON [dbo].[Member]([Tag] ASC);
+
---
+++ ClansHuntApp.Database/dbo/Tables/MemberStatus.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[MemberStatus] (
+    [MemberID]          INT      NOT NULL,
+    [DonationsGiven]    INT      NULL,
+    [DonationsReceived] INT      NULL,
+    [CreatedDate]       DATETIME CONSTRAINT [DF_MemberStatus_CreatedDate] DEFAULT (getdate()) NOT NULL,
+    CONSTRAINT [FK_MemberStatus_Member] FOREIGN KEY ([MemberID]) REFERENCES [dbo].[Member] ([ID]) ON DELETE CASCADE
+);
+
